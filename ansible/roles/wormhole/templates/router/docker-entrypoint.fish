104540e76b09e7ad4d504c74425c0a3fac724754
Andrei Borisov <andrei@borisov.dev>
2026-04-18 18:58:06 +0300

    🚧 Migrate to host networking to restore Tailscale direct connectivity

--- ansible/roles/wormhole/templates/router/docker-entrypoint.fish
+++ ansible/roles/wormhole/templates/router/docker-entrypoint.fish
@@ -1,25 +1,6 @@
 #!/usr/bin/fish
 
-if test (count $argv) -eq 0
-  echo "Usage: docker-entrypoint.fish <interface> [interface2 ...]"
-  exit 1
-end
-
-set interfaces $argv
-
-function wait_for_interfaces
-  echo "Waiting for interfaces:" $interfaces
-
-  for interface in $interfaces
-    while not ip link show $interface &> /dev/null
-      sleep 5
-    end
-    echo "Interface $interface is up."
-  end
-end
-
 function apply_rules
-  echo "Applying rules..."
   echo "Applying nftables rules..."
   nft -f /etc/nftables/ruleset.nft
   or return 1
@@ -27,23 +8,4 @@ function apply_rules
   echo "Done."
 end
 
-function monitor_interfaces
-  echo "Monitoring interfaces:" $interfaces
-
-  ip monitor link | while read -l line
-    for interface in $interfaces
-      if string match -q "Deleted*$interface*" $line
-        echo "Interface $interface removed."
-        # Drain the pipe to let ip monitor exit cleanly
-        kill (pgrep -n ip) 2> /dev/null
-        return 1
-      end
-    end
-  end
-end
-
-while true
-  wait_for_interfaces
-  and apply_rules
-  and monitor_interfaces
-end
+apply_rules
